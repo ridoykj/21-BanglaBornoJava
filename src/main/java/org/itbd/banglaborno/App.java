008f106b6ef9c99377f256b8f8bdc579ea1b100f
ridoy.kumar <ridoy.kumar@gmail.com>
2024-03-06 22:00:13 +0600

    init source code

---
+++ src/main/java/org/itbd/banglaborno/App.java
@@ -0,0 +1,13 @@
+package org.itbd.banglaborno;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/org/itbd/BanglaBorno/AppTest.java
@@ -0,0 +1,10 @@
+package org.itbd.BanglaBorno;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testApp() {
+        System.out.println("Hello World!");
+    }
+}
